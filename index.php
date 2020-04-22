3101c059949efd16bfb647bb38ceb00dd6f5da03
tarq-p <pierluigi.tarquinio90@gmail.com>
2020-04-22 15:47:52 +0200

    creazione files

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="css/master.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/d3js/5.15.1/d3.min.js"></script>
+        <title>php-adv-charts</title>
+    </head>
+    <body>
+        <?php echo "ciao"; ?>
+
+    <script src="js/main.js" charset="utf-8"></script>
+    </body>
+</html>
